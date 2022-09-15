92f1541f2b29bd9d5b9cdcbccd6a74886545c58d
Na-Rae Kim <narae1096@gmail.com>
2022-09-15 11:13:01 +0100

    update grammar.checker.rb

--- phase_two/lib/grammar_checker.rb
+++ phase_two/lib/grammar_checker.rb
@@ -1,7 +1,8 @@
 def grammar_checker(text)
   fail "This is not a sentence." if text.empty?
-  punctuation_check = [".", "!", "?"].include? text[-1]
-  if text[0] == text[0].upcase && punctuation_check
+  first_character_is_capital = text[0] == text[0].upcase
+  last_character_is_punctuation = [".", "!", "?"].include?(text[-1])
+  if first_character_is_capital && last_character_is_punctuation
     return "Correct!"
   else
     return "WRONG! Check your grammar."
