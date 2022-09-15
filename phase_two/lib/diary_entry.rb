e777cd24657fa91c13c579655435a2b089f6bb4d
Na-Rae Kim <narae1096@gmail.com>
2022-09-15 14:42:41 +0100

    exercise of test drive a class

---
+++ phase_two/lib/diary_entry.rb
@@ -0,0 +1,45 @@
+class DiaryEntry
+  def initialize(title, contents) #title, contents are strings
+    @title = title
+    @contents = contents
+    @next_word_read = 0
+  end
+
+  def title
+    return @title
+  end
+
+  def contents
+    return @contents 
+  end
+
+  def count_words
+    return words.length
+  end
+
+  def reading_time(wpm)
+    fail "Reading speed must be above zero" unless wpm.positive?
+    return (count_words / wpm.to_f).ceil
+  end
+  
+  def reading_chunk(wpm, minutes) 
+    fail "Reading speed must be above zero" unless wpm.positive?
+    words_user_can_read = wpm * minutes
+    start_from = @next_word_read
+    end_at = @next_word_read + words_user_can_read 
+    words_chunck = words[start_from, end_at]
+    if end_at >= count_words
+      @next_word_read = 0
+    else
+      @next_word_read = end_at
+    end 
+    return words_chunck.join(" ")
+  end 
+  
+  private
+
+  def words
+    return @contents.split(" ")
+  end
+end 
+
---
+++ phase_two/spec/diary_entry_spec.rb
@@ -0,0 +1,75 @@
+require './lib/diary_entry.rb'
+
+RSpec.describe DiaryEntry do
+  it "constructs" do
+    diary_entry = DiaryEntry.new("title", "contents")
+    expect(diary_entry.title).to eq "title"
+    expect(diary_entry.contents).to eq "contents"
+  end
+  
+  describe "count_words method" do
+    it "counts zero for an empty string" do
+      diary_entry = DiaryEntry.new("title", "")
+      expect(diary_entry.count_words).to eq 0
+    end 
+
+    it "counts the number of words in the contents" do
+      diary_entry = DiaryEntry.new("title", "contents are here")
+      expect(diary_entry.count_words).to eq 3
+    end   
+  end
+  
+  describe "reading_time method" do
+    context "given a zero wpm" do
+      it "fails" do
+        diary_entry = DiaryEntry.new("title", "contents" * 180)
+        expect{ diary_entry.reading_time(0) }.to raise_error "Reading speed must be above zero"
+      end
+    end
+
+    context "given wpm of a reasonalbe number such as 200" do 
+      it "returns estimated reading time in minutes" do
+        diary_entry = DiaryEntry.new("title", "contents " * 600)
+        expect(diary_entry.reading_time(200)).to eq 3
+      end
+    end
+  end
+
+  describe "reading_chunk method" do
+    context "with a text readable with in the given minutes" do
+      it "returns a full chunck" do
+        diary_entry = DiaryEntry.new("title", "one two three")
+        text_chunk = diary_entry.reading_chunk(200, 1)
+        expect(text_chunk).to eq ("one two three")  
+      end
+
+      it "returns a readable chunck" do
+        diary_entry = DiaryEntry.new("title", "one two three")
+        text_chunk = diary_entry.reading_chunk(2, 1)
+        expect(text_chunk).to eq ("one two")  
+      end
+
+      it 'returns the next chunck, next time we are asked' do
+        diary_entry = DiaryEntry.new("title", "one two three")
+        diary_entry.reading_chunk(2, 1)
+        text_chunk = diary_entry.reading_chunk(2, 1)
+        expect(text_chunk).to eq "three"
+      end
+
+      it "restarts after reading the whole countents" do
+        diary_entry = DiaryEntry.new("title", "one two three")
+        diary_entry.reading_chunk(2, 1)
+        diary_entry.reading_chunk(1, 1)
+        text_chunk = diary_entry.reading_chunk(2, 1)
+        expect(text_chunk).to eq "one two"
+      end
+    end
+
+    context "with given 0 wmp" do
+      it "fails" do
+        diary_entry = DiaryEntry.new("title", "one two three")
+        expect{ diary_entry.reading_chunk(0,2) }.to raise_error "Reading speed must be above zero" 
+      end
+    end
+  end
+end 
