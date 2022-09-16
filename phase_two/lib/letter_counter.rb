66cd7da9a81841b86a33d22c758f6d37578a2ebb
Na-Rae Kim <narae1096@gmail.com>
2022-09-16 16:39:24 +0100

    puts if statement to check output

--- phase_two/lib/letter_counter.rb
+++ phase_two/lib/letter_counter.rb
@@ -4,7 +4,7 @@ class LetterCounter
   end
 
   def calculate_most_common()
-    counter = Hash.new(0)
+    counter = Hash.new(1)
     most_common = nil
     most_common_count = 1
     @text.chars.each do |char|
@@ -12,8 +12,8 @@ class LetterCounter
       next unless is_letter?(char)
       counter[char] = (counter[char] || 1) + 1
       if counter[char] > most_common_count
-        most_common = char
-        most_common_count += counter[char]
+        p most_common = char
+        p most_common_count += counter[char]
       end
     end
     return [most_common_count, most_common]
